45308b39a4626dc59a58950a4544818220d1c8a2
Janu Kleyn Fourie <s219295301@mandela.ac.za>
2022-04-14 14:15:08 +0200

    ADD: Object Model

--- src/page/productDetails/productDetailsView.js
+++ src/page/productDetails/productDetailsView.js
@@ -8,12 +8,78 @@ import { UilCheckCircle } from '@iconscout/react-unicons'
 function ProductDetailsView() {
   const baseURL = "https://fe-assignment.vaimo.net/";
 
-  const [imgSource, setImgSource] = useState([]);
+  const [responseObject, setResponseObject]=useState({
+    name: '',
+    tags: [],
+    options: [{
+      label: '',
+      price: {
+        value: 0,
+        currency: {
+          code: '',
+          symbol: '',
+          format: ''
+        }
+      },
+      old_price: {
+        value: 0,
+        currency: {
+          code: '',
+          symbol: '',
+          format: ''
+        }
+      }
+    }
+  ],
+  discount: {
+    amount: '',
+    end_date: new Date()
+  },
+  gallery: [
+    {
+      main: ''
+    }
+  ],
+  shipping: {
+    method: {
+      country: '',
+      title: '',
+      shipping_time: {
+        value: '',
+        info: ''
+      },
+      cost: {
+        value: 0,
+        currency: {
+          code: '',
+          symbol: '',
+          format: ''
+        }
+      }
+    },
+    lead_time: {
+      value: '',
+      info: ''
+    },
+    props: {
+      ready_to_ship: true,
+      in_stock: false,
+      fast_dispatch: true
+    }
+  },
+  reviews: {
+    rating: 0,
+    count: 0,
+    total_buyers: 0
+  }
+})
 
   const productInformation = useCallback(() => {
     axios.get(baseURL).then((res) => {
-      setImgSource(res.data.product.gallery[0].main);
+      setResponseObject(res.data.product);
     });
+
+    console.log(responseObject);
   });
 
   useEffect(() => {
@@ -22,12 +88,12 @@ function ProductDetailsView() {
 
   return (
     <div className="container">
-      <ProductImage source={imgSource} />
+      <ProductImage source={responseObject.gallery[0].main} />
       <div className='info-container'>
         <div className="badges">
-          <div className="ready-to-ship"><span>Ready To Ship</span></div>
-          <div className="in-stock"><UilCheckCircle className='badge-icon'/><span>In Stock</span></div>
-          <div className="fast-dispatch"><UilCheckCircle className='badge-icon'/><span>Fast Dispatch</span></div>
+          {responseObject.shipping.props.ready_to_ship === true && <div className="ready-to-ship"><span>Ready To Ship</span></div>}
+          {responseObject.shipping.props.in_stock === true && <div className="in-stock"><UilCheckCircle className='badge-icon'/><span>In Stock</span></div>}
+          {responseObject.shipping.props.fast_dispatch === true && <div className="fast-dispatch"><UilCheckCircle className='badge-icon'/><span>Fast Dispatch</span></div>}
         </div>
         </div>  
       <div className="add-container"></div>
